3924441228e548c8f19fafcf1e14ef3d96cf2764
Jon Carbonell <navatante@proton.me>
2024-03-03 23:43:34 +0100

    Update MainProgram.java

--- Bucles_03/Aplicacion_03/_3_13/MainProgram.java
+++ Bucles_03/Aplicacion_03/_3_13/MainProgram.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Inserte la hora: ");
         int hora = Integer.parseInt(scanner.nextLine());
